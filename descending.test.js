25ce760e128c0161a9c78b06ac18d4c7a67ee17e
ijw9209 <ijw9209@naver.com>
2020-04-23 16:45:23 +0900

    정수 내림차순으로 배치 완료

---
+++ descending.test.js
@@ -0,0 +1,20 @@
+solution = n => {
+    let answer = 0;
+    const arr = [];
+    let str = String(n);
+    for (let i = 0; i < str.length; i++) {
+        arr[i] = str[i];
+    }
+    const sortArr = arr.sort(function (a, b) {
+        return b - a;
+    });
+
+    answer = sortArr.join("");
+    return Number(answer);
+}
+
+
+
+test('정수 내림차순으로 배치하기', () => {
+    expect(solution(118372)).toEqual(873211);
+})
