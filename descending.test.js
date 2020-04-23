2144bbf8ea7938b1619b3be2931925e833230078
ijw9209 <ijw9209@naver.com>
2020-04-23 17:04:03 +0900

    정수 내림차순으로 배치 완료

--- descending.test.js
+++ descending.test.js
@@ -13,8 +13,19 @@ solution = n => {
     return Number(answer);
 }
 
-
-
 test('정수 내림차순으로 배치하기', () => {
     expect(solution(118372)).toEqual(873211);
 })
+
+
+//다른사람
+// function solution(n) {
+//     const newN = n + "";
+//     const newArr = newN
+//       .split("")
+//       .sort()
+//       .reverse()
+//       .join("");
+
+//     return +newArr;
+//   }
