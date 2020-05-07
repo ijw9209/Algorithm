1ca86f1141c47492c5397d6b8b580761ab5ca0af
ijw9209 <ijw9209@naver.com>
2020-05-07 19:36:17 +0900

    행렬의 덧셈 완료

--- processionPlus.test.js
+++ processionPlus.test.js
@@ -1,12 +1,24 @@
 solution = (arr1, arr2) => {
 
-    const answer = arr1.map((a, i) => a.map((b, j) => b + arr2[i][j]));
+
+    var answer = [];
+    let len1 = arr1.length;
+    let len2 = arr1[0].length;
+
+    for (let i = 0; i < len1; i++) {
+        answer.push([]);
+    }
+
+    for (let i = 0; i < len1; i++) {
+        for (let j = 0; j < len2; j++) {
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
 
     return answer;
 
-
 }
 
-test('같은 숫자는 싫어', () => {
+test('행렬의 덧셈', () => {
     expect(solution([[1, 2], [2, 3]], [[3, 4], [5, 6]])).toEqual([[4, 6], [7, 9]]);
 })
